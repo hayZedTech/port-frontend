299f582eb505e8a0f8c24b71b5ea789ec7f6e77d
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-08-23 06:55:04 +0100

    Initial commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -19,7 +19,7 @@ import node from "./Images/node.png";
 import cbt_image from "./Images/cbt_image.png";
 import chat_img from "./Images/chat_img.jpg";
 import ecommerce from "./Images/ecommerce.png";
-// import MessageForm from "./MessageForm";
+import App from "./MessageForm";
 import { Footer } from "./Layout";
 
 
@@ -314,7 +314,7 @@ export const Home = () => {
 
       <div>
                 <br /><br />
-                <MessageForm />
+                <App />
         </div>
 
     
